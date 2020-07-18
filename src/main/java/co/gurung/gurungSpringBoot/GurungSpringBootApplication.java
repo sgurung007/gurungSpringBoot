c5c757a7b894cea2907b7b56bcc2fc3ed8a826c1
surajGurung <sgrg007@gmail.com>
2020-07-18 01:07:50 -0500

    Initial commit

---
+++ src/main/java/co/gurung/gurungSpringBoot/GurungSpringBootApplication.java
@@ -0,0 +1,13 @@
+package co.gurung.gurungSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GurungSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GurungSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/gurung/gurungSpringBoot/GurungSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package co.gurung.gurungSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GurungSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
